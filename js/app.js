ccb22215ab00ad35b5fa0f47b56f961f951871b0
Mugi-Star <90766470+Mugi-Star@users.noreply.github.com>
2022-10-17 18:20:53 +0300

    2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide')
 
-const clearActiveClasses = () => {
+  slides[activeSlide].classList.add('slide_active')
+
+  const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+      slide.classList.remove('slide_active')
+    })
+  }
+
   slides.forEach((slide) => {
-    slide.classList.remove('slide_active')
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+
+      slide.classList.add('slide_active')
+    })
   })
 }
 
-slides.forEach((slide) => {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-
-    slide.classList.add('slide_active')
-  })
-})
+slidesPlugin(3)
